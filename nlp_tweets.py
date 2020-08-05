f28a94849446bc18039fb17d7ebf8227213df726
Tyler Woods <tyjordan.woods@gmail.com>
2020-08-04 20:46:32 -0700

    add Secret Keys class attributes to access tweepy api

--- nlp_tweets.py
+++ nlp_tweets.py
@@ -1,6 +1,23 @@
+# import the necessary packages
+import os 
+import tweepy as tw 
+import pandas as pd 
+
+# import class that contains secret keys
+from secret_keys import Secret_Keys
+
+# initiliaze a class from secret_keys that contains
+# attributes for the secret keys
+keys = Secret_Keys()
+
+auth = tw.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+auth.set_access_token(keys.access_token, keys.access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+
+
 ## to make a new environment, python -m venv env
 ## to activate environment, do source env/bin/activate
 ## to get out of virtual environment, do deactivate
 ## to delete the environment, rm -rf env
-## https://www.pyimagesearch.com/2020/05/25/tesseract-ocr-text-localization-and-detection/
-
+## https://www.earthdatascience.org/courses/use-data-open-source-python/intro-to-apis/twitter-data-in-python/
