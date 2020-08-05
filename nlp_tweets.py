e998047e488ba27ed672e76e2f7721e41d226a95
Tyler Woods <tyjordan.woods@gmail.com>
2020-08-04 20:35:22 -0700

    edit .gitignore

---
+++ nlp_tweets.py
@@ -0,0 +1,6 @@
+## to make a new environment, python -m venv env
+## to activate environment, do source env/bin/activate
+## to get out of virtual environment, do deactivate
+## to delete the environment, rm -rf env
+## https://www.pyimagesearch.com/2020/05/25/tesseract-ocr-text-localization-and-detection/
+
