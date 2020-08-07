12145a2fb9ceec8ea2573fc36c3e641c0702c89d
Tyler Woods <tyjordan.woods@gmail.com>
2020-08-06 19:39:18 -0700

    modify imports

--- nlp_tweets.py
+++ nlp_tweets.py
@@ -13,11 +13,3 @@ keys = Secret_Keys()
 auth = tw.OAuthHandler(keys.consumer_key, keys.consumer_secret)
 auth.set_access_token(keys.access_token, keys.access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
-
-
-
-## to make a new environment, python -m venv env
-## to activate environment, do source env/bin/activate
-## to get out of virtual environment, do deactivate
-## to delete the environment, rm -rf env
-## https://www.earthdatascience.org/courses/use-data-open-source-python/intro-to-apis/twitter-data-in-python/
